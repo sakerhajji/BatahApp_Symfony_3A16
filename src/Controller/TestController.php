220dc699c4916b663a864cddb3a719f5f25e5dc1
saker <saker.hajji13@gmail.com>
2024-03-25 10:24:15 +0100

    add page loginPage.html.twig and page singnUp.html.twig and integration front.html.twig

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,24 +2,21 @@
 
 namespace App\Controller;
 
-use App\Repository\PartenairesRepository;
-use App\Repository\UtilisateurRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 class TestController extends AbstractController
 {
-
-    public function __construct(private PartenairesRepository $repository)
+    #[Route('/signUp', name: 'signUp')]
+    public function signUp(): Response
     {
+        return $this->render('utilisateur/loginPage.html.twig');
     }
 
     #[Route('/home', name: 'app_test')]
     public function index(): Response
     {
-       return $this->render('base.html.twig');
- //       dd($this->repository->findAll()) ;
+        return $this->render('base.html.twig');
     }
 }
