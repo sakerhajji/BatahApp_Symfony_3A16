a0213d4e0928c06152ea37a276e5336d82b6c447
sofienneee <sofienne.mrabet@esprit.tn>
2024-05-01 19:24:38 +0100

    mise a jour front

--- src/Form/ServiceApresVenteTypeEdit.php
+++ src/Form/ServiceApresVenteTypeEdit.php
@@ -2,31 +2,45 @@
 
 namespace App\Form;
 
+use App\Entity\Partenaires;
 use App\Entity\ServiceApresVente;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\ChoiceList\Loader\CallbackChoiceLoader;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ServiceApresVenteTypeEdit extends AbstractType
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('description')
             ->add('type', ChoiceType::class, [
                 'choices' => [
-                    // Add your choices here
-                    'voiture ' => 'voiture',
-                    'terrain ' => 'terrain',
-                    // Add more choices as needed
+                    'voiture' => 'voiture',
+                    'terrain' => 'terrain',
                 ],
             ])
             ->add('date')
             ->add('status')
             ->add('achats')
-             ->add('idPartenaire')
-        ;
+            ->add('idPartenaire', EntityType::class, [
+                'class' => Partenaires::class,
+                'choice_label' => 'nom',
+                'placeholder' => 'Sélectionner un partenaire',
+
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
